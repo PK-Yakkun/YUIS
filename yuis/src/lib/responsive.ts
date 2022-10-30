c9db2aa9927e9dc0c39e209efa83edaa8a2ce556
PK-Yakkun <mokkun515@gmail.com>
2022-10-30 17:12:05 +0900

    feat: 1propsで複数のCSSを与えられるように拡張

--- yuis/src/lib/responsive.ts
+++ yuis/src/lib/responsive.ts
@@ -11,12 +11,26 @@ import { css } from "@emotion/react";
  * @returns ブレイクポイントごとのオブジェクトCSS
  */
 export const createResponsiveStyle = (
-  propaty: string,
-  sm?: CSS.Properties,
-  md?: CSS.Properties
+  propaty?: string | string[],
+  sm?: CSS.Properties | string | number,
+  md?: CSS.Properties | string | number
 ) => {
-  return css(
-    sm != null && theme.breakpoint.sm({ [propaty]: sm }),
-    md != null && theme.breakpoint.md({ [propaty]: md })
-  );
+  if (typeof propaty === "string") {
+    // propatyがstringの場合
+    return css(
+      sm != null && theme.breakpoint.sm({ [propaty]: sm }),
+      md != null && theme.breakpoint.md({ [propaty]: md })
+    );
+  } else if (Array.isArray(propaty)) {
+    // propatyが配列の場合
+    return css(
+      sm != null && theme.breakpoint.sm({ [propaty![0]]: sm }),
+      theme.breakpoint.sm({ [propaty![1]]: sm }),
+      md != null && theme.breakpoint.md({ [propaty![0]]: md }),
+      theme.breakpoint.md({ [propaty![1]]: md })
+    );
+  } else {
+    // あり得ない条件、処理の終了
+    return;
+  }
 };
