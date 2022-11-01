d0be34153f39e4cb556ed5a22e005b83594fadee
PK-Yakkun <mokkun515@gmail.com>
2022-11-02 01:59:50 +0900

    feat: Themeのカラーを変更

--- yuis/src/theme/settings/palettes.ts
+++ yuis/src/theme/settings/palettes.ts
@@ -1,13 +1,13 @@
 export const palette = {
   body: "#fff",
-  primary: "#9a9fac",
+  primary: "#74bfaf",
   secondary: "#f3f3f3",
   typography: "#5f626c",
 };
 
 export const nightPalette = {
   body: "#202124",
-  primary: "#50535a",
+  primary: "#4cbfa6",
   secondary: "#ebebeb",
   typography: "#8b8f9d",
 };
