ad237441faacf38cdc1a0b39bbf7b0452062f69f
PK-Yakkun <mokkun515@gmail.com>
2022-11-02 02:00:19 +0900

    feat: グローバルにThemeを適応

--- yuis/src/pages/_app.tsx
+++ yuis/src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
-import { theme } from "@/theme/theme";
+import { ThemeContext } from "@/lib/store/theme";
+import { theme, nightTheme } from "@/theme/theme";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // NightModeに切り替えるステート
+  const [isNightMode, setIsNightMode] = useState<boolean>(false);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+    // <ThemeProvider theme={isNightMode ? nightTheme : theme}>
+    <ThemeProvider theme={isNightMode ? nightTheme : theme}>
+      <ThemeContext.Provider value={{ isNightMode, setIsNightMode }}>
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
     </ThemeProvider>
   );
 }
