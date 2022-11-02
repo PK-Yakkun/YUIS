b0b548bac65bfc3fc265beaf9e867b5bbe17c7d1
PK-Yakkun <mokkun515@gmail.com>
2022-11-03 01:40:26 +0900

    add: Plus, MinusIconコンポーネント作成

---
+++ yuis/src/components/atoms/Icons/MinusIcon.tsx
@@ -0,0 +1,7 @@
+export const MinusIcon = () => {
+  return (
+    <svg fill="#fff" viewBox="0 0 24 24">
+      <path d="M18 11h-12c-1.104 0-2 0.896-2 2s0.896 2 2 2h12c1.104 0 2-0.896 2-2s-0.896-2-2-2z" />{" "}
+    </svg>
+  );
+};
