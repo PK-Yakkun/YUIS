daeef4fceb4992b49b519782635bc0777a77c8a0
PK-Yakkun <mokkun515@gmail.com>
2022-11-03 01:39:12 +0900

    feat: BoxコンポーネントのbgColorにthemeを適応

--- yuis/src/components/atoms/Box/Box.tsx
+++ yuis/src/components/atoms/Box/Box.tsx
@@ -6,16 +6,35 @@ import { OpacityProps, opacityMixin } from "@/styles/mixins/opacity";
 import { PositionProps, positionMixin } from "@/styles/mixins/position";
 import { SpaceProps, spaceMixin } from "@/styles/mixins/space";
 import { AllEventType } from "@/types/events";
+import { ColorType } from "@/types/color";
+
+type BoxBgProps = Partial<BackgroundProps>;
 
 export type BoxStyleProps = Partial<LayoutProps> &
   Partial<SpaceProps> &
-  Partial<BackgroundProps> &
+  Omit<BoxBgProps, "bgColor"> &
   Partial<BorderProps> &
   Partial<PositionProps> &
   Partial<OpacityProps> &
-  Partial<AllEventType>;
+  Partial<AllEventType> & {
+    bgColor?: ColorType;
+  };
 
 export const BoxStyled = styled.div<BoxStyleProps>(
+  ({ theme, bgColor }) => {
+    switch (bgColor) {
+      case "body":
+        return { backgroundColor: theme.color.body };
+      case "primary":
+        return { backgroundColor: theme.color.primary };
+      case "secondary":
+        return { backgroundColor: theme.color.secondary };
+      case "typography":
+        return { backgroundColor: theme.color.typography };
+      default:
+        return null;
+    }
+  },
   layoutMixin,
   spaceMixin,
   backgroundMixin,
