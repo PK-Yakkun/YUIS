41d0b68d9c990c804a956250d11958c3b81d4b04
PK-Yakkun <mokkun515@gmail.com>
2022-11-03 12:47:06 +0900

    feat: Title追加

--- yuis/src/pages/_app.tsx
+++ yuis/src/pages/_app.tsx
@@ -4,15 +4,18 @@ import { ThemeProvider } from "@emotion/react";
 import { ThemeContext } from "@/lib/store/theme";
 import { theme, nightTheme } from "@/theme/theme";
 import { useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // NightModeに切り替えるステート
   const [isNightMode, setIsNightMode] = useState<boolean>(false);
 
   return (
-    // <ThemeProvider theme={isNightMode ? nightTheme : theme}>
     <ThemeProvider theme={isNightMode ? nightTheme : theme}>
       <ThemeContext.Provider value={{ isNightMode, setIsNightMode }}>
+        <Head>
+          <title>YUIS</title>
+        </Head>
         <Component {...pageProps} />
       </ThemeContext.Provider>
     </ThemeProvider>
