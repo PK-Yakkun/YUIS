66b01bd07a1dd79ce5510a5816650cf3eb6f5510
PK-Yakkun <mokkun515@gmail.com>
2022-11-03 12:46:30 +0900

    add: favicon設定

---
+++ yuis/src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* favicon設定 */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/images/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
